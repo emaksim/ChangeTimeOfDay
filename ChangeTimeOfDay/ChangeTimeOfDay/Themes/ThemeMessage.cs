14b11a8323b5a95fe1c8d5f9f423efa864d48aba
emaksim <eg92saratov@mail.ru>
2021-09-29 13:56:55 +0400

    Change time of day
     xamarin app

---
+++ ChangeTimeOfDay/ChangeTimeOfDay/Themes/ThemeMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeTimeOfDay.Themes
+{
+    class ThemeMessage
+    {
+        public const string ThemeChanged =  "ThemeChanged";
+    }
+}
